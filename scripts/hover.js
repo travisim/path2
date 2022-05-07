d9d5c7519d71cd568c28efe0c2be79dd3e9e0216
Chong Tze Zhao <chong.tzezhao@dhs.sg>
2022-05-07 20:52:18 +0800

    removed redundant code

--- scripts/hover.js
+++ scripts/hover.js
@@ -3,24 +3,6 @@ myUI.handle_map_hover = function(e){
 	let y = e.offsetY;
 	let scaled_x = Math.floor(x/myUI.canvases.hover_map.canvas.clientWidth *  myUI.map_width);
 	let scaled_y = Math.floor(y/myUI.canvases.hover_map.canvas.clientHeight *  myUI.map_height);
-	/*if (myUI.map_goal_icon.clicked){
-		//  means released
-		myUI.map_goal_icon.elem.style.top = (y-15).toString() + "px";
-		myUI.map_goal_icon.elem.style.left = (x-15).toString() + "px";
-		myUI.map_goal_icon.clicked = false;
-		myUI.map_goal = [scaled_y, scaled_x];
-		myUI.displayScen();
-		return
-	}
-	else if (myUI.map_start_icon.clicked){
-		//  means released
-		myUI.map_start_icon.elem.style.top = (y-15).toString() + "px";
-		myUI.map_start_icon.elem.style.left = (x-15).toString() + "px";
-		myUI.map_start_icon.clicked = false;
-		myUI.map_start = [scaled_y, scaled_x];
-		myUI.displayScen();
-		return
-	}*/
 	myUI.hover_labels.hover_x.elem.innerHTML = scaled_x;
 	myUI.hover_labels.hover_y.elem.innerHTML = scaled_y;
 	myUI.canvases.hover_map.erase_canvas();
@@ -36,21 +18,7 @@ myUI.canvases.hover_map.canvas.addEventListener(`mousemove`, myUI.handle_map_hov
 
 myUI.canvases.hover_map.canvas.addEventListener(`mouseout`, e=>myUI.canvases.hover_map.erase_canvas());
 
-/*myUI.map_goal_icon.elem.addEventListener(`mousedown`, e=>{
-	myUI.map_goal_icon.clicked = true;
-});
 
-myUI.map_goal_icon.elem.addEventListener(`mouseup`, e=>{
-	myUI.map_goal_icon.clicked = false;
-});
-
-myUI.map_start_icon.elem.addEventListener(`mousedown`, e=>{
-	myUI.map_start_icon.clicked = true;
-});
-
-myUI.map_start_icon.elem.addEventListener(`mouseup`, e=>{
-	myUI.map_start_icon.clicked = false;
-});*/
 dragElement(myUI.map_start_icon.elem);
 dragElement(myUI.map_goal_icon.elem);
 
