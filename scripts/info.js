fa5ff2edef2d931ea577742f71ec39f2a92147f1
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2022-05-15 15:40:30 +0000

    updated info.js

--- scripts/info.js
+++ scripts/info.js
@@ -1,5 +1,3 @@
- 
-document.getElementById("currentYX").innerHTML = "(1024,1024)"; 
 
 
 
@@ -20,29 +18,30 @@ var info_neighbours_id = ["NW","N","NE","W","E","SW","S","SE"];
 for(var i=0;i<info_neighbours_id.length;++i){
   document.getElementById(info_neighbours_id[i]).innerHTML = 'F:<span class "F_cost" id="F"></span>G:<span id="G"></span>H:<span id="H"></span>Type:<span id="type"></span>';
 };
+/*
+function get_RGB_point(id,x,y){
+var RGB_string = "rgb(${document.getElementById("id").getImageData(x, y, 1, 1).data[0]},${document.getElementById("id").getImageData(x, y, 1, 1).data[1]},${document.getElementById("id").getImageData(x, y, 1, 1).data[2]})";
+}
 
+*/
+    
+  
+
+
+
+//demo
 document.querySelector("#NW").querySelector("#F").innerHTML = "13.13";
 
 
+document.getElementById("NW").style.borderColor = "rgb(0,0,0)";//obstacle
+document.getElementById("N").style.borderColor = "rgb(221,48,33)";//visited
 
-/*
-document.getElementById("NW").style.backgroundColor ="rgb(116,250,76)";
-document.getElementById("N").style.backgroundColor ="rgb(221,48,33)";
-document.getElementById("NE").style.backgroundColor ="rgb(52,119,234)";
-document.getElementById("W").style.backgroundColor ="rgb(30,73,25)";
-*/
-/*["queue", "rgb(116,250,76)"],
-    ["visited", "rgb(221,48,33)"],
-    ["current_YX", "rgb(52,119,234)"],
-    ["neighbours", "rgb(30,73,25)"],
-    */
-document.getElementById("NW").style.borderColor = "rgb(0,0,0)";
-document.getElementById("N").style.borderColor = "rgb(221,48,33)";
-
-document.getElementById("W").style.borderColor = "rgb(0,130,105)";
+document.getElementById("W").style.borderColor = "rgb(0,130,105)";//neighbour
 
 
 
+ 
+document.getElementById("currentYX").innerHTML = "(1024,1024)"; 
 
 
  table = document.getElementById("info_table");
@@ -54,5 +53,3 @@ for(var i=1;i<5;++i){
   }
 
 } 
-
-l
