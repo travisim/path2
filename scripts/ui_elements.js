b8949b1d45ae2ca5ed9b6ac45c690be018465ee9
Travis Sim <64481469+travisim@users.noreply.github.com>
2022-09-09 05:02:07 +0000

    further updates to css

--- scripts/ui_elements.js
+++ scripts/ui_elements.js
@@ -444,3 +444,20 @@ if (CP && CP.lineTo) {
     this.restore();
   }
 }
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active_Section");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } 
+    else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
