44c345a80ab3f2e9ff15c933ee63ac262d7cce59
Chong Tze Zhao <chong.tzezhao@dhs.sg>
2022-09-17 01:19:03 +0800

    fixed indentation

--- scripts/step.js
+++ scripts/step.js
@@ -120,105 +120,94 @@ myUI.run_steps = function(num_steps, step_direction="fwd", combined=false){
       if(myUI.testing) console.log([STATIC_COMMANDS[command], STATIC_DESTS[dest], x, y, parentX, parentY, stepNo, arrowIndex, gCost, hCost]);
       if(gCost!==undefined && hCost!==undefined) var fCost=(gCost+hCost).toPrecision(5);
       console.log("cmd",STATIC_COMMANDS[command],"dest", statics_to_obj[dest],"x", x, "y", y, "f",fCost,"g",gCost,"h",hCost,parentX,parentY,'stepno', stepNo,'pseudoCodeRow', pseudoCodeRow);
-        if(command==STATIC.SIMPLE && findSimple>=0){
-          if(findSimple==1) findSimple--;
-          else{
-            if(step_direction=="fwd") --myUI.animation.step;
-            else{
-              ++myUI.animation.step;
-              myUI.run_steps(1, "bck");
-            }
-            return;
-          }
-        }
-        else if(command==STATIC.EC){
-          myUI.canvases[statics_to_obj[dest]].erase_canvas();
-        }
-        else if(command==STATIC.DP){
-          myUI.canvases[statics_to_obj[dest]].draw_pixel([x,y]);
-        }
-        else if(command==STATIC.EP){
-            myUI.canvases[statics_to_obj[dest]].erase_pixel([x,y]);
-        }
-        else if(command==STATIC.INC_P){
-            myUI.canvases[statics_to_obj[dest]].change_pixel([x,y], "inc");
-          
-        }
-        else if(command==STATIC.DEC_P){
-            myUI.canvases[statics_to_obj[dest]].change_pixel([x,y], "dec");
-        }
-        else if(command==STATIC.DA){
-          // draw arrow
-          myUI.arrow.elems[arrowIndex].classList.remove("hidden");
-          myUI.arrow.elems[arrowIndex].style.fill = myUI.arrow.colors[colorIndex];
-        }
-        else if(command==STATIC.EA){
-          // erase arrow
-          myUI.arrow.elems[arrowIndex].classList.add("hidden");
-        }
+      if(command==STATIC.EC){
+        myUI.canvases[statics_to_obj[dest]].erase_canvas();
+      }
+      else if(command==STATIC.DP){
+        myUI.canvases[statics_to_obj[dest]].draw_pixel([x,y]);
+      }
+      else if(command==STATIC.EP){
+          myUI.canvases[statics_to_obj[dest]].erase_pixel([x,y]);
+      }
+      else if(command==STATIC.INC_P){
+          myUI.canvases[statics_to_obj[dest]].change_pixel([x,y], "inc");
         
-          if(dest==STATIC.CR && command == STATIC.EP ){//record  "visiters" in 2d array
-            myUI.InfoMap.recordErasedVisited(x,y);            	            
-          }
-          if(dest== STATIC.QU && command == STATIC.EP ){//record  "visiters" in 2d array
-            myUI.InfoMap.recordErasedQueue(x,y);
-          }
-          if(command == STATIC.DICR   && dest==STATIC.ICR){//draw "current_XY",
-            myUI.InfoMap.reset();
-            myUI.InfoMap.drawObstacle(x,y);
-            myUI.InfoMap.drawOutOfBound(x,y);
-            myUI.InfoMap.drawVisited(x,y);
-            myUI.InfoMap.drawQueue(x,y);
-            myUI.InfoCurrent.DrawCurrent(x,y);
-          }
-    
-          
-          //to draw neighbors
-          else if(command == STATIC.DIM){
+      }
+      else if(command==STATIC.DEC_P){
+          myUI.canvases[statics_to_obj[dest]].change_pixel([x,y], "dec");
+      }
+      else if(command==STATIC.DA){
+        // draw arrow
+        myUI.arrow.elems[arrowIndex].classList.remove("hidden");
+        myUI.arrow.elems[arrowIndex].style.fill = myUI.arrow.colors[colorIndex];
+      }
+      else if(command==STATIC.EA){
+        // erase arrow
+        myUI.arrow.elems[arrowIndex].classList.add("hidden");
+      }
       
-            myUI.InfoNWSE[statics_to_obj[dest]].drawOneNeighbour(fCost,gCost,hCost);
-          
-            
-          }
+      if(dest==STATIC.CR && command == STATIC.EP ){//record  "visiters" in 2d array
+        myUI.InfoMap.recordErasedVisited(x,y);            	            
+      }
+      if(dest== STATIC.QU && command == STATIC.EP ){//record  "visiters" in 2d array
+        myUI.InfoMap.recordErasedQueue(x,y);
+      }
+      if(command == STATIC.DICR   && dest==STATIC.ICR){//draw "current_XY",
+        myUI.InfoMap.reset();
+        myUI.InfoMap.drawObstacle(x,y);
+        myUI.InfoMap.drawOutOfBound(x,y);
+        myUI.InfoMap.drawVisited(x,y);
+        myUI.InfoMap.drawQueue(x,y);
+        myUI.InfoCurrent.DrawCurrent(x,y);
+      }
+
+      
+      //to draw neighbors
+      else if(command == STATIC.DIM){
+  
+        myUI.InfoNWSE[statics_to_obj[dest]].drawOneNeighbour(fCost,gCost,hCost);
+      
         
-          else if(command == STATIC.InTop && dest==STATIC.IT){
-            myUI.InfoTable.inTop(stepNo,[stepNo,x+", "+y,parentX+", "+parentY,fCost,gCost,hCost]);                
-          }
-          else if(command == STATIC.InBottom && dest==STATIC.IT){
-            myUI.InfoTable.inBottom(stepNo,[stepNo,x+", "+y,parentX+", "+parentY,fCost,gCost,hCost]);                
-          }
-          else if(command == STATIC.OutTop && dest==STATIC.IT){
-            myUI.InfoTable.outTop();             
-          }
-          else if(command == STATIC.OutBottom && dest==STATIC.IT){
-            myUI.InfoTable.outBottom();             
-          }
-          else if(command == STATIC.Sort){
-            if (myUI.InfoTable.rows.length >= 2){
-              myUI.InfoTable.sort(); // emulats insert at based on F cost
-            }
-          }
-          //to erase neighbors
-          else if(command == STATIC.EIM ){
-            myUI.InfoNWSE[statics_to_obj[dest]].resetOne();
-        
-          }
-        
-        if(dest==STATIC.CR && command == STATIC.DP ){//record  "visiters" in 2d array
-          myUI.InfoMap.recordDrawnVisited(x,y);            	            
+      }
+    
+      else if(command == STATIC.InTop && dest==STATIC.IT){
+        myUI.InfoTable.inTop(stepNo,[stepNo,x+", "+y,parentX+", "+parentY,fCost,gCost,hCost]);                
+      }
+      else if(command == STATIC.InBottom && dest==STATIC.IT){
+        myUI.InfoTable.inBottom(stepNo,[stepNo,x+", "+y,parentX+", "+parentY,fCost,gCost,hCost]);                
+      }
+      else if(command == STATIC.OutTop && dest==STATIC.IT){
+        myUI.InfoTable.outTop();             
+      }
+      else if(command == STATIC.OutBottom && dest==STATIC.IT){
+        myUI.InfoTable.outBottom();             
+      }
+      else if(command == STATIC.Sort){
+        if (myUI.InfoTable.rows.length >= 2){
+          myUI.InfoTable.sort(); // emulats insert at based on F cost
         }
-        if(dest == STATIC.QU && command == STATIC.DP ){//record  "visiters" in 2d array
-          myUI.InfoMap.recordDrawnQueue(x,y);
-        }
-        if(dest == STATIC.IT && command == STATIC.RemoveRowByID ){//record  "visiters" in 2d array
-          myUI.InfoTable.removeRowById(stepNo);
-        }  
-        if(dest == STATIC.PC && command == STATIC.HighlightPseudoCodeRowPri ){//record  "visiters" in 2d array
-          myUI.PseudoCode.highlightPri(pseudoCodeRow);
-        }  
-        if(dest == STATIC.PC && command == STATIC.HighlightPseudoCodeRowSec ){//record  "visiters" in 2d array
-          myUI.PseudoCode.highlightSec(pseudoCodeRow);
-        }  
+      }
+      //to erase neighbors
+      else if(command == STATIC.EIM ){
+        myUI.InfoNWSE[statics_to_obj[dest]].resetOne();
+    
+      }
+      
+      if(dest==STATIC.CR && command == STATIC.DP ){//record  "visiters" in 2d array
+        myUI.InfoMap.recordDrawnVisited(x,y);            	            
+      }
+      if(dest == STATIC.QU && command == STATIC.DP ){//record  "visiters" in 2d array
+        myUI.InfoMap.recordDrawnQueue(x,y);
+      }
+      if(dest == STATIC.IT && command == STATIC.RemoveRowByID ){//record  "visiters" in 2d array
+        myUI.InfoTable.removeRowById(stepNo);
+      }  
+      if(dest == STATIC.PC && command == STATIC.HighlightPseudoCodeRowPri ){//record  "visiters" in 2d array
+        myUI.PseudoCode.highlightPri(pseudoCodeRow);
+      }  
+      if(dest == STATIC.PC && command == STATIC.HighlightPseudoCodeRowSec ){//record  "visiters" in 2d array
+        myUI.PseudoCode.highlightSec(pseudoCodeRow);
+      }  
       try{
       }catch(e){
         console.log(e);
