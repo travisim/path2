184dac121c59c6ea70cc72136ac9bda5d5c51117
Travis Sim <64481469+travisim@users.noreply.github.com>
2022-10-15 05:45:39 +0000

    added pseudo code for PRM and RRT

--- scripts/pathfinding/Bresenhams_line_generation_algorithm.js
+++ scripts/pathfinding/Bresenhams_line_generation_algorithm.js
@@ -1,10 +1,11 @@
+ 
 function Bresenham(x0, y0, x1, y1) {
         let dots = [];
         let dx = Math.abs(x1 - x0);
         let dy = Math.abs(y1 - y0);
         let sx = (x0 < x1) ? 1 : -1;
         let sy = (y0 < y1) ? 1 : -1;
-        let err = dx - dy;
+        let err = dx - dy; // err should be zero
 
         dots.push([x0,y0]);
 
@@ -16,8 +17,8 @@ function Bresenham(x0, y0, x1, y1) {
                 x0 += sx;
             }
 
-            if (e2 < dx) {
-                err += dx;
+            if (e2 < dx) { 
+                err += dx; 
                 y0 += sy;
             }
 
@@ -27,7 +28,10 @@ function Bresenham(x0, y0, x1, y1) {
         return dots;
     }
 
+//https://www.cs.helsinki.fi/group/goa/mallinnus/lines/bresenh.html
+//http://members.chello.at/~easyfilter/Bresenham.pdf refer to page 14 of this
 
-var x1 = 0, y1 = 0, x2 = 4, y2 =12; 
+
+var x1 = 6, y1 = 0, x2 = 0, y2 =12; 
  console.log(Bresenham(x1, y1, x2, y2))
 myUI.canvases["path"].draw_canvas(Bresenham(x1, y1, x2, y2), `1d`, false, false);
