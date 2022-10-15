4db896ac370ec30de9a0303e0809cd8ee326a361
Chong Tze Zhao <chong.tzezhao@dhs.sg>
2022-10-15 18:52:53 +0800

    fixed bug

--- scripts/interface.js
+++ scripts/interface.js
@@ -199,7 +199,7 @@ myUI.initialize = function(){
   myUI.InfoTables = {};
   myUI.InfoTables["ITQueue"] = new UIInfoTable("Queue"); // do not shift to top as prerequisite required
   myUI.InfoTables["ITQueue"].setTableActive();
-  myUI.Infotables["ITQueue"].setTableHeader(["Queue No","Vertex","Parent","F cost","G cost","H cost"]);
+  myUI.InfoTables["ITQueue"].setTableHeader(["Queue No","Vertex","Parent","F cost","G cost","H cost"]);
   myUI.InfoMap  = new UIInfoMap();
   myUI.PseudoCode = new UIInfoPseudoCode();
   //myUI.pseudoCodeRaw = 'def astar(map, start_vertex, goal_vertex): \nlist = OpenList() \npath = [ ] \n#Initialise h-cost for all \nfor vertex in map.vertices(): \n    vertex.set_h_cost(goal_vertex)  \n    vertex.g_cost = ∞  \n    vertex.visited = False \n  # Assign 0 g-cost to start_vertex  \n start_vertex.g_cost = 0 \n list.add(start_vertex) \n while list.not_empty(): \n  current_vertex = list.remove() \n  # Skip if visited: a cheaper path  \n  # was already found \n    if current_vertex.visited: \n      continue \n   # Trace back and return the path if at the goal \n   if current_vertex is goal_vertex : \n     while current_vertex is not None: \n      path.push(current_vertex) \n      current_vertex = current_vertex.parent \n     return path # exit the function \n  # Add all free, neighboring vertices which \n   # are cheaper, into the list  \n  for vertex in get_free_neighbors(map, current_vertex):  \n      # f or h-costs are not checked bcos obstacles \n     # affects the optimal path cost from the g-cost \n     tentative_g = calc_g_cost(vertex, current_vertex)  \n     if tentative_g < vertex.g_cost: \n       vertex.g_cost = tentative_g  \n      vertex.parent = current_vertex  \n      list.add(vertex) \nreturn path';
@@ -214,8 +214,8 @@ myUI.initialize();
  
 myUI.export = function(){
   // run in inspect element console
-  console.log(JSON.stringify(myUI.canvasArray));
-  console.log(JSON.stringify(myUI.hoverArray));
+  console.log("CANVAS: ",JSON.stringify(myUI.canvasArray));
+  console.log("HOVER: ",JSON.stringify(myUI.hoverArray));
   console.log("DEST ENUMS WRITING: ",JSON.stringify(STATIC_DESTS));
   console.log("DEST ENUMS READING: ",JSON.stringify(statics_to_obj));
   console.log("INFOTABLE: ",JSON.stringify(myUI.infotableArray));
