777b01f034fa98400043b48d48df796d076d4863
Travis Sim <64481469+travisim@users.noreply.github.com>
2023-08-05 03:26:29 +0800

    map axis scales with canvas size

--- scripts/canvasAxis.js
+++ scripts/canvasAxis.js
@@ -1,27 +1,43 @@
 
 function drawAxis(mapHeight = 16, mapWidth = 16) {
-     largerDimension = Math.max(myUI.map_width,myUI.map_height);
-    if (largerDimension < 32) {
+    largerDimension = Math.max(myUI.map_width, myUI.map_height);
+    
+    if (true) {
 
 
 
        
-        var grid_size = 472 * 2 / largerDimension;
+        var largerDimensionOfGrid = 472 * 2 / largerDimension;
+        var grid_size_Y = largerDimensionOfGrid;
+        var grid_size_X = largerDimensionOfGrid;
+
         console.log(myUI.map_width,myUI.map_height,"width height")
 
-        var x_axis_distance_grid_lines = 0;
-        var y_axis_distance_grid_lines = 0;
-        var x_axis_starting_point = { number: 1, suffix: '' };
-        var y_axis_starting_point = { number: 1, suffix: '' };
-
+        
         var canvas = document.getElementById("my-canvas");
         var ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        
         var canvas_width = canvas.width;
         var canvas_height = canvas.height;
+        
+        var temp_num_lines_x = Math.floor(canvas_height / largerDimensionOfGrid) * (myUI.map_height / largerDimension);
+        scaleObjY = reduceBelowXbyDividingK(temp_num_lines_x, 5, 20)
+        var num_lines_x = scaleObjY.new_x
+        grid_size_Y = grid_size_Y * scaleObjY.cnt
+        
+        var temp_num_lines_y = Math.floor(canvas_width / largerDimensionOfGrid) * (myUI.map_width / largerDimension);
+        scaleObjX = reduceBelowXbyDividingK(temp_num_lines_y, 5, 20)
+        var num_lines_y = scaleObjX.new_x
+        grid_size_X = grid_size_X * scaleObjX.cnt
 
-        var num_lines_x = Math.floor(canvas_height / grid_size)*(myUI.map_height/largerDimension);
-        var num_lines_y = Math.floor(canvas_width / grid_size)* (myUI.map_width/largerDimension);
+        var x_axis_distance_grid_lines = 0;
+        var y_axis_distance_grid_lines = 0;
+        var x_axis_starting_point = { number: scaleObjX.cnt, suffix: '' };
+        var y_axis_starting_point = { number: scaleObjY.cnt, suffix: '' };
+        // var temp_num_lines_y = Math.floor(canvas_width / grid_size_Y) * (myUI.map_width / largerDimension);
+        // var num_lines_y = Math.floor(canvas_width / grid_size_X)* (myUI.map_width/largerDimension);
 
         // Draw grid lines along X-axis
         for (var i = 0; i <= num_lines_x; i++) {
@@ -35,12 +51,12 @@ function drawAxis(mapHeight = 16, mapWidth = 16) {
                 ctx.strokeStyle = "#e9e9e9";
     
             if (i == num_lines_x) {
-                ctx.moveTo(0, grid_size * i);
-                ctx.lineTo(canvas_width, grid_size * i);
+                ctx.moveTo(0, grid_size_Y * i);
+                ctx.lineTo(canvas_width, grid_size_Y * i);
             }
             else {
-                ctx.moveTo(0, grid_size * i + 0.5);
-                ctx.lineTo(canvas_width, grid_size * i + 0.5);
+                ctx.moveTo(0, grid_size_Y * i + 0.5);
+                ctx.lineTo(canvas_width, grid_size_Y * i + 0.5);
             }
             ctx.stroke();
         }
@@ -57,18 +73,18 @@ function drawAxis(mapHeight = 16, mapWidth = 16) {
                 ctx.strokeStyle = "#e9e9e9";
     
             if (i == num_lines_y) {
-                ctx.moveTo(grid_size * i, 0);
-                ctx.lineTo(grid_size * i, canvas_height);
+                ctx.moveTo(grid_size_X * i, 0);
+                ctx.lineTo(grid_size_X * i, canvas_height);
             }
             else {
-                ctx.moveTo(grid_size * i + 0.5, 0);
-                ctx.lineTo(grid_size * i + 0.5, canvas_height);
+                ctx.moveTo(grid_size_X * i + 0.5, 0);
+                ctx.lineTo(grid_size_X * i + 0.5, canvas_height);
             }
             ctx.stroke();
         }
 
         // Translate to the new origin. Now Y-axis of the canvas is opposite to the Y-axis of the graph. So the y-coordinate of each element will be negative of the actual
-        ctx.translate(y_axis_distance_grid_lines * grid_size, x_axis_distance_grid_lines * grid_size);
+        ctx.translate(y_axis_distance_grid_lines * grid_size_Y, x_axis_distance_grid_lines * grid_size_X);
 
         // Ticks marks along the positive X-axis
         for (i = 1; i < (num_lines_y - y_axis_distance_grid_lines); i++) {
@@ -77,8 +93,8 @@ function drawAxis(mapHeight = 16, mapWidth = 16) {
             ctx.strokeStyle = "#ffffff";
 
             // Draw a tick mark 6px long (-3 to 3)
-            ctx.moveTo(grid_size * i + 0.5, -3);
-            ctx.lineTo(grid_size * i + 0.5, 3);
+            ctx.moveTo(grid_size_X * i + 0.5, -3);
+            ctx.lineTo(grid_size_X * i + 0.5, 3);
             ctx.stroke();
 
             // Text value at that point
@@ -87,7 +103,7 @@ function drawAxis(mapHeight = 16, mapWidth = 16) {
             ctx.font = '18px Arial';
             ctx.textAlign = 'start';
             ctx.fillStyle = "#ffffff";
-            ctx.fillText(x_axis_starting_point.number * i + x_axis_starting_point.suffix, grid_size * i + 7, 25);
+            ctx.fillText(x_axis_starting_point.number * i + x_axis_starting_point.suffix, grid_size_X * i + 7, 25);
         }
 
         // Ticks marks along the negative X-axis
@@ -97,15 +113,15 @@ function drawAxis(mapHeight = 16, mapWidth = 16) {
             ctx.strokeStyle = "#ffffff";
 
             // Draw a tick mark 6px long (-3 to 3)
-            ctx.moveTo(-grid_size * i + 0.5, -3);
-            ctx.lineTo(-grid_size * i + 0.5, 3);
+            ctx.moveTo(-grid_size_X * i + 0.5, -3);
+            ctx.lineTo(-grid_size_X * i + 0.5, 3);
             ctx.stroke();
 
             // Text value at that point
             ctx.font = '18px Arial';
             ctx.textAlign = 'end';
             ctx.fillStyle = "#ffffff";
-            ctx.fillText(-x_axis_starting_point.number * i + x_axis_starting_point.suffix, -grid_size * i + 7, 25);
+            ctx.fillText(-x_axis_starting_point.number * i + x_axis_starting_point.suffix, -grid_size_X * i + 7, 25);
         }
 
         // Ticks marks along the positive Y-axis
@@ -116,8 +132,8 @@ function drawAxis(mapHeight = 16, mapWidth = 16) {
             ctx.strokeStyle = "#ffffff";
 
             // Draw a tick mark 6px long (-3 to 3)
-            ctx.moveTo(-3, grid_size * i + 0.5);
-            ctx.lineTo(3, grid_size * i + 0.5);
+            ctx.moveTo(-3, grid_size_Y * i + 0.5);
+            ctx.lineTo(3, grid_size_Y * i + 0.5);
             ctx.stroke();
 
             // Text value at that point
@@ -125,7 +141,7 @@ function drawAxis(mapHeight = 16, mapWidth = 16) {
             ctx.textAlign = 'start';
             ctx.fillStyle = "#ffffff";
 
-            ctx.fillText(y_axis_starting_point.number * i + y_axis_starting_point.suffix, 8, grid_size * i + 19);
+            ctx.fillText(y_axis_starting_point.number * i + y_axis_starting_point.suffix, 8, grid_size_Y * i + 19);
         }
 
         // Ticks marks along the negative Y-axis
@@ -136,8 +152,8 @@ function drawAxis(mapHeight = 16, mapWidth = 16) {
             ctx.strokeStyle = "#ffffff";
 
             // Draw a tick mark 6px long (-3 to 3)
-            ctx.moveTo(-3, -grid_size * i + 0.5);
-            ctx.lineTo(3, -grid_size * i + 0.5);
+            ctx.moveTo(-3, -grid_size_Y * i + 0.5);
+            ctx.lineTo(3, -grid_size_Y * i + 0.5);
             ctx.stroke();
 
             // Text value at that point
@@ -145,7 +161,26 @@ function drawAxis(mapHeight = 16, mapWidth = 16) {
             ctx.textAlign = 'start';
             ctx.fillStyle = "#ffffff";
 
-            ctx.fillText(y_axis_starting_point.number * i + y_axis_starting_point.suffix, 8, -grid_size * i + 17);
+            ctx.fillText(y_axis_starting_point.number * i + y_axis_starting_point.suffix, 8, -grid_size_Y * i + 17);
         }
     }
+}
+function reduceBelowXbyDividingK(x, k, max) {
+    cnt = 1
+    while (true) {
+        if (x < max) {
+            console.log(x, "x")
+            return {
+                new_x: x,
+                cnt: cnt
+                
+            }
+            // x = x / k
+            
+        }
+        x = Math.floor(x / k)
+        cnt = cnt*k
+    }
+
+    
 }
